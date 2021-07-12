2f904b83c4233a9bc6eb672353da219321528caa
Alissa Vining <ajv5282@psu.edu>
2021-07-11 20:18:15 -0400

    Added craft page and functionality

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 if (process.env.LIVE_RELOAD == "true") require("./modules/reload")(__dirname);
 
 let aws = new $AWS(
-    "AKIA4US7MEW7BUKOFLU7", 
+    "AKIA4US7MEW7BUKOFLU7",
     "QdYKyQnoIJ/3PZ9tRMWJxObiYfrEC1SJ3euGGnVc"
 )
 
 var mainWindow;
 
-app.on("ready", function (){
+app.on("ready", function () {
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
@@ -30,7 +30,7 @@ app.on("ready", function (){
         })
     );
 
-    mainWindow.on("closed", function (){
+    mainWindow.on("closed", function () {
         app.quit();
     });
 
@@ -38,19 +38,26 @@ app.on("ready", function (){
 
     mainWindow.setMenuBarVisibility(false);
 
+    //Book
     ipcMain.on("AddBook", AddBook);
-    ipcMain.on("EditBooks", UpdateBook);
+    ipcMain.on("EditBook", UpdateBook);
     ipcMain.on("NeedBooks", GrabAllBooks);
     ipcMain.on("DeleteBook", DeleteBook)
 
-    function GrabAllBooks(){
+    //Craft
+    ipcMain.on("AddCraft", AddCraft);
+    ipcMain.on("EditCraft", UpdateCraft);
+    ipcMain.on("NeedCrafts", GrabAllCrafts);
+    ipcMain.on("DeleteCraft", DeleteCraft)
+
+    function GrabAllBooks() {
         aws.GetAllItems("Table", (err, data) => {
             if (err) console.log(err)
             else mainWindow.webContents.send("HeresBooks", data)
         })
     }
 
-    function AddBook(event, formData){
+    function AddBook(event, formData) {
         let item = {
             BookName: formData.BookName,
             BookAuthor: formData.BookAuthor,
@@ -59,7 +66,10 @@ app.on("ready", function (){
 
         aws.AddItem("Table", item, (err, data) => {
             if (err) console.log(err)
-            else console.log(data)
+            else {
+                console.log(data)
+                GrabAllBooks()
+            }
         });
     }
 
@@ -81,6 +91,56 @@ app.on("ready", function (){
 
     function DeleteBook(event, formData) {
         aws.DeleteItem("Table", formData.PrimaryKey, (err, data) => {
+            if (err) console.log(err)
+            else console.log(data)
+        });
+    }
+
+    function AddCraft(event, formData) {
+        console.log(formData.CraftDescription)
+        let item = {
+            CraftName: formData.CraftName,
+            CraftSupplies: formData.CraftSupplies,
+            CraftDescription: formData.CraftDescription,
+            CraftImage: formData.CraftImage
+        };
+
+        console.log(item)
+        aws.AddItem("Craft", item, (err, data) => {
+            if (err) console.log(err)
+            else {
+                console.log(data)
+                GrabAllCrafts();
+            }
+        });
+    }
+
+    function GrabAllCrafts() {
+        aws.GetAllItems("Craft", (err, data) => {
+            if (err) console.log(err)
+            else mainWindow.webContents.send("HeresCrafts", data)
+        })
+    }
+
+    function UpdateCraft(event, formData) {
+        let item = {
+            CraftName: formData.CraftName,
+            CraftSupplies: formData.CraftSupplies,
+            CraftDescription: formData.CraftDescription,
+            CraftImage: formData.CraftImage
+        };
+
+        aws.UpdateItem("Craft", item, formData.PrimaryKey, (err, data) => {
+            if (err) console.log(err)
+            else {
+                console.log(data)
+                GrabAllCrafts()
+            }
+        });
+    }
+
+    function DeleteCraft(event, formData) {
+        aws.DeleteItem("Craft", formData.PrimaryKey, (err, data) => {
             if (err) console.log(err)
             else console.log(data)
         });
